c7c429d05e3f8c0213c0c7fa3bdde2cbf72208d0
FVS-MBA <feliven@gmail.com>
2026-04-25 11:29:11 -0300

    salva chave do token JWT em variável de ambiente

--- backend/src/utils/gerarJwt.ts
+++ backend/src/utils/gerarJwt.ts
@@ -1,9 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const chaveSecreta = "chaveSuperSecreta";
-
 export const gerarJwt = (payload: object) => {
-  return jwt.sign(payload, chaveSecreta, { expiresIn: "1h" });
+  return jwt.sign(payload, process.env.SEGREDO_JWT ?? "", { expiresIn: "1h" });
 };
 
 // const tokenVerificado = jwt.verify(token, chaveSecreta);
